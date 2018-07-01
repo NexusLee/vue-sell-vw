e9815d2933e9c7767b99434c300feb2497d0cbd7
NexusLee <tonexus.lee@gmail.com>
2018-07-01 21:38:30 +0800

    scss

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="header">
-    <div class="background">
-      <img :src="seller.avatar" style="width:64px;height:64px" />
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" style="width:64px;height:64px" />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}} 分钟送达
+        </div>
+        <div v-if="seller.supports" class="supports">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
     </div>
+    <div class="bulltin-wrapper"></div>
+
   </div>
 </template>
 
@@ -23,3 +40,7 @@
     }
   }
 </script>
+
+<style scoped>
+  @import "./header.scss";
+</style>
