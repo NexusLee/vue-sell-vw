fd82168f17ac998b1b2b47f26140fafc84d0ac88
NexusLee <tonexus.lee@gmail.com>
2018-07-04 13:55:56 +0800

    样式改为750视口

--- src/App.vue
+++ src/App.vue
@@ -65,13 +65,13 @@ export default {
 <style>
   @import "./common/scss/index.scss";
 
-  #app {
+#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 html {
   margin: 0;
