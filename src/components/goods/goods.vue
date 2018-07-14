cbfb73462f89f40ee5cd8d46945d0a31a7ba890c
NexusLee <tonexus.lee@gmail.com>
2018-07-14 22:06:56 +0800

    商品页滚动效果

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item, index) in goods" class="menu-item" :class="{'current':currentIndex==index}" @click="selectMenu(index, $event)">
           <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list" ref="foodList">
           <h1 class="title">{{item.name}}</h1>
           <ul v-for="food in item.foods" class="food-item">
             <div class="icon">
@@ -21,12 +21,10 @@
               <h2 class="name">{{food.name}}</h2>
               <p class="desc">{{food.description}}</p>
               <div class="extra">
-                <span class="count">月售{{food.sellCount}}份</span>
-                <span>好评率{{food.rating}}%</span>
+                <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
               </div>
               <div class="price">
-                <span class="now">¥{{food.price}}</span>
-                <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
               </div>
             </div>
           </ul>
@@ -38,17 +36,33 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 200;
   export default {
     name: 'goods',
     props: {
-      sellere: {
+      seller: {
         type: Object
       }
     },
     data () {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
+      }
+    },
+    computed: {
+      currentIndex() {
+        for(let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
       }
     },
     created() {
@@ -58,8 +72,44 @@
       this.axios.get(url).then((response) => {
         if(response.status === ERR_OK) {
           this.goods = Object.assign({}, this.goods, response.data.goods);
+          this.$nextTick(() => {
+            this._initScroll();
+            this._calculateHeight();
+      })
         }
       })
+    },
+    methods: {
+      selectMenu(index, event) {
+        if(!event._constructed) {
+          return;
+        }
+        let foodList = this.$refs.foodList;
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+        console.log(index)
+      },
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        })
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodList;
+        let height = 0;
+        this.listHeight.push(height);
+        for(let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      }
     }
   }
 </script>
@@ -67,5 +117,4 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
   @import "./goods.scss";
-
 </style>
