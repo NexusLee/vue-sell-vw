2dfe19f9ec3f06ced63eb63dfbd56a3eec4b3898
NexusLee <tonexus.lee@gmail.com>
2018-07-26 20:51:01 +0800

    food detail

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,16 +1,35 @@
 <template>
   <transition name="move">
-    <div v-show="showFlag" class="food">
+    <div v-show="showFlag" class="food" ref="food">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image" />
+          <div class="back" @click="hide">
+            <i class="icon-arrow_lift"></i>
+          </div>
         </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}%</span>
+          </div>
+          <div class="price">
+            <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+          </div>
+        </div>
+        <div class="cartcontrol-wrapper" >
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div class="buy"></div>
       </div>
     </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+  import cartcontrol from '../cartcontrol/cartcontrol';
   export default {
     props: {
       food: {
@@ -25,26 +44,26 @@
     methods: {
       show() {
        this.showFlag = true;
+        this.$nextTick(() => {
+          if(!this.scroll) {
+            this.scroll = new BScroll(this.$refs.food, {
+              click: true
+            })
+          }else{
+            this.scroll.refresh();
+          }
+        })
+      },
+      hide() {
+        this.showFlag = false;
       }
+    },
+    components: {
+      cartcontrol
     }
   }
 </script>
 
 <style lang="scss">
-  .food {
-    position: fixed;
-    left: 0;
-    top: 0;
-    bottom: 48px;
-    width: 100%;
-    background-color: #ffffff;
-    z-index: 30;
-    transform: translate3d(0, 0, 0);
-    &.move-enter, &.move-leave-active {
-      transform: translate3d(100%, 0, 0);
-    }
-    &.move-enter-active, &.move-leave-active {
-      transition: all 0.2s linear;
-    }
-  }
+  @import "./food.scss";
 </style>
