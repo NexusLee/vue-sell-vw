28c2f0e2603f5d625c1fe681fe5872f0614e00ee
NexusLee <tonexus.lee@gmail.com>
2018-08-03 21:10:48 +0800

    ratingselect

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+      <span class="block positive" :class="{'active': selectType===2}">{{desc.all}}<span class="count">47</span></span>
+      <span class="block positive" :class="{'active': selectType===0}">{{desc.positive}}<span class="count">10</span></span>
+      <span class="block negative" :class="{'active': selectType===1}">{{desc.negative}}<span class="count">7</span></span>
     </div>
     <div class="switch">
       <span class="icon-check_cicle"></span>
